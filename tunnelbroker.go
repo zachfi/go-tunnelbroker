af5b5283987b4aa3013f0d731f58b0ee3ea210f6
Zach Leslie <xaque208@gmail.com>
2022-05-16 15:46:08 +0000

    chore: implement NewClient() and adjust pointer references

--- tunnelbroker.go
+++ tunnelbroker.go
@@ -2,6 +2,7 @@ package tunnelbroker
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -17,7 +18,6 @@ type Tunnels struct {
 }
 
 type Tunnel struct {
-	ID          int    `xml:"id,attr"`
 	Description string `xml:"description"`
 	ServerV4    string `xml:"serverv4"`
 	ClientV4    string `xml:"clientv4"`
@@ -25,28 +25,44 @@ type Tunnel struct {
 	Clientv6    string `xml:"clientv6"`
 	Routed64    string `xml:"routed64"`
 	Routed48    string `xml:"routed48"`
+	ID          int    `xml:"id,attr"`
 }
 
-func (c *Client) TunnelInfo() (Tunnels, error) {
+func NewClient(username, password *string) (*Client, error) {
+	if username == nil || password == nil {
+		return nil, fmt.Errorf("username and password must not be nil")
+	}
+
+	if *username == "" || *password == "" {
+		return nil, fmt.Errorf("username and password must not be empty")
+	}
+
+	return &Client{
+		Username: *username,
+		Password: *password,
+	}, nil
+}
+
+func (c *Client) TunnelInfo() (*Tunnels, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://tunnelbroker.net/tunnelInfo.php", nil)
 	req.SetBasicAuth(c.Username, c.Password)
 	resp, err := client.Do(req)
 	if err != nil {
-		return Tunnels{}, err
+		return &Tunnels{}, err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return Tunnels{}, err
+		return &Tunnels{}, err
 	}
 
-	tunnels := Tunnels{}
-	err = xml.Unmarshal(body, &tunnels)
+	tunnels := &Tunnels{}
+	err = xml.Unmarshal(body, tunnels)
 	if err != nil {
-		return Tunnels{}, err
+		return &Tunnels{}, err
 	}
 
 	return tunnels, nil
